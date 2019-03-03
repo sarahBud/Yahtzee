f4962a82580bb7f0a17a6557b3ea4d573910c390
sebudwell@gmail.com <sebudwell@gmail.com>
2019-03-03 11:11:48 -0600

    Add project files.

---
+++ Yahtzee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yahtzee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
